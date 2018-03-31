48360bc459dade10f129b2e917aa7445b0a1f450
Menno <menno.vrahden@gmail.com>
2018-03-31 12:58:38 +0200

    minor fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Point2D } from './utils/Point2D';
 
 import { World } from './World';
-import { WorldObject } from "./components/WorldObject";
+import { WorldObject } from './components/WorldObject';
 import { Item } from './components/Item';
 import { Wall } from './components/Wall';
 import { RLAgent } from './components/agents/RLAgent';
@@ -10,4 +10,4 @@ import { Lidar } from './components/agents/components/Lidar';
 import { Radar } from './components/agents/components/Radar';
 import { SensedObject } from './components/agents/components/utils/SensedObject';
 
-export { World, Item, Wall, RLAgent, SensedObject, Point2D, Lidar, Radar, WorldObject };
+export { World, WorldObject, Item, Wall, RLAgent, SensedObject, Point2D, Lidar, Radar };
